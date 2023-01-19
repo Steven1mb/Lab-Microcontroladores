56552fccf1b3ff1493199d1d8a0e69809aa20b32
Steven <stevenmora109@gmail.com>
2023-01-19 12:33:38 -0600

    algunos cambios en el codigo

--- src/blink.c
+++ src/blink.c
@@ -1,6 +1,25 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define LED_rojo      PB0
+#define LED_verde     PB1
+#define LED_amarillo  PB2
+#define LED_azul      PB3
+
+#define boton_rojo      PC0
+#define boton_verde     PC1
+#define boton_amarillo  PC2
+#define boton_azul      PC3
+
+int nivel = 0 // Nivel actual
+
+void init() {
+    // Pongo los pins de los leds como salidas
+    DDRB = (1 << LED_rojo) | (1 << LED_verde) | (1 << LED_amarillo) | (1 << LED_azul);
+
+    // Pongo los pins de los bottones como entradas
+    DDRC = 0;
+}
 int main(void)
 {
 
