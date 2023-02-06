eab2f6ba8d6665a8f4036ec9cc0af5bb5e50b587
Adrián Avilés Flores <aviles2122@gmail.com>
2023-02-05 19:29:58 -0600

    Comunicacion serial

--- src/lab3arduino/lab3arduino.ino
+++ src/lab3arduino/lab3arduino.ino
@@ -14,11 +14,16 @@
 Adafruit_PCD8544 display = Adafruit_PCD8544(SCLK, DIN, DC, CS, RST);
 
 // Declaro los pines con un nombre 
-const int entrada4 = A0;
-const int entrada3 = A1;
-const int entrada2 = A2;
-const int entrada1 = A3;
+const int entrada4 = A3;
+const int entrada3 = A2;
+const int entrada2 = A1;
+const int entrada1 = A0;
 const int switchpin = 2;
+const int serialpin = 7;
+
+float v1, v2, v3, v4;
+char mode[20];
+int estadoswitch;
 
 void setup() {
 
@@ -29,18 +34,17 @@ void setup() {
   display.display();
   
   // Inicializar pines de entrada y salida
-  pinMode(switchPin, INPUT);
-
+  pinMode(switchpin, INPUT);
+  pinMode(serialpin, INPUT);
+  pinMode(12, OUTPUT);
 
   // Inicializar el puerto serial
   Serial.begin(9600);
 
-
 }
 
 void loop() {
-  int estadoswitch = digitalRead(switchpin);
-  int v1, v2, v3, v4;
+  estadoswitch = digitalRead(switchpin);
 
   // Leer voltaje de los 4 canales
   v4 = analogRead(entrada4);
@@ -53,7 +57,36 @@ void loop() {
   v3 = (v3 * 5.0) / 1023.0;
   v2 = (v2 * 5.0) / 1023.0;
   v1 = (v1 * 5.0) / 1023.0;
+
   // Cuando el switch esta bajo es en DC
-    if (estadoswitch == LOW) {
+  if (estadoswitch == LOW) {
     // Modo DC
+    v4 = (v4 * 9.6) - 24; // Estas 4 señales se escalan segun
+    v3 = (v3 * 9.6) - 24; // el divisor de tensiones aplicado
+    v2 = (v2 * 9.6) - 24; // y se restan los 24V de la bateria
+    v1 = (v1 * 9.6) - 24; // permanente para hallar valor real
+    strcpy(mode,"DC");
+  }
+  else { // Modo AC
+    v4 = ((v4 * 4.56) + 1.2) / 1.4142; // Estas 4 señales primero se escalan
+    v3 = ((v3 * 4.56) + 1.2) / 1.4142; // segun el divisor de tensiones, luego
+    v2 = ((v2 * 4.56) + 1.2) / 1.4142; // se compensa la caida de tension de
+    v1 = ((v1 * 4.56) + 1.2) / 1.4142; // los 2 diodos y se dividen por raiz de 2
+    strcpy(mode,"AC");
+  }
+  
+  // Un switch habilita la comunicacion serial,
+  // se envian los datos de los 4 canales
+  if (digitalRead(serialpin)) {
+    Serial.print(mode); Serial.print(",");
+    Serial.print(v1); Serial.print(" V,");
+    Serial.print(v2); Serial.print(" V,");
+    Serial.print(v3); Serial.print(" V,");
+    Serial.print(v4); Serial.println(" V");
+    delay(1000);
+    digitalWrite(12, LOW);
+  }
+  else {
+    digitalWrite(12, HIGH);
+  }
 }
