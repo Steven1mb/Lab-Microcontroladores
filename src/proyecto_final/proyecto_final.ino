d3ca82f1f7b3c80aaeed63b2cf08fca825959a6b
Steven1mb <stevenmora109@gmail.com>
2023-02-26 17:05:03 -0600

    Implementar LCD
    Se muestran valores de humedad y temperatura en la pantalla LCD.

--- src/proyecto_final/proyecto_final.ino
+++ src/proyecto_final/proyecto_final.ino
@@ -1,16 +1,19 @@
 #include "dht.h"
+#include <LiquidCrystal_I2C.h>
 #define soil_apin A0 // El higrómetro está conectado al PIN A0
 #define dht_apin A1 // El DHT11 está conectado al PIN A1
  
 dht DHT;
- 
+LiquidCrystal_I2C lcd(0x27,16,2); 
+
 void setup(){
  
   Serial.begin(9600);
-  delay(500);// Delay para dejar que el sistema se inicie
-  Serial.println("Sensor DHT11 de temperatura y humedad \n\n");
   delay(1000);// Esperar para accesar el sensor
-}//end "setup()"
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+}
  
 void loop(){
 
@@ -25,7 +28,18 @@ void loop(){
     Serial.print("Temperatura actual = ");
     Serial.print(DHT.temperature);
     Serial.println("°C");
-    
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Humedad:"); 
+    lcd.setCursor(9,0);
+    lcd.print(Moisture); 
+    lcd.setCursor (0,1);
+    lcd.print("Temp: ");
+    lcd.setCursor(9,1);
+    lcd.print(DHT.temperature);
+    lcd.display();
+
     delay(3000);// Delay para accesar sensores dentro de 3 segundos
  
 }//  Fin del programa
