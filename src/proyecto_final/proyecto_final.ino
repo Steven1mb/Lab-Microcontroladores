0bada835bfdcdc16276397bba53ab84834ddca1c
Steven1mb <stevenmora109@gmail.com>
2023-02-26 19:49:32 -0600

    Hora de último riego
    Se muestra en LCD la hora de último riego.

--- src/proyecto_final/proyecto_final.ino
+++ src/proyecto_final/proyecto_final.ino
@@ -2,12 +2,18 @@
 #include <LiquidCrystal_I2C.h>
 #define soil_apin A0 // El higrómetro está conectado al PIN A0
 #define dht_apin A1 // El DHT11 está conectado al PIN A1
+#define pin_agua 2 // Relay de la valvula con PIN 2
+#define pin_abanico 4 // Relay del ventilador con PIN 4
+
+unsigned long t_1 = 0;
+unsigned long t_2 = 0;
+float t_min = 0;
  
 dht DHT;
-LiquidCrystal_I2C lcd(0x27,16,2); 
+LiquidCrystal_I2C lcd(0x27,16,2);
 
-const int pin_agua = 2;
-const int pin_abanico = 4;
+void regar_planta();
+
 void setup(){
  
   Serial.begin(9600);
@@ -17,6 +23,8 @@ void setup(){
   lcd.clear();
   pinMode(pin_agua, OUTPUT);
   pinMode(pin_abanico, OUTPUT);
+  digitalWrite(pin_agua, LOW);
+  digitalWrite(pin_abanico, LOW);
 }
  
 void loop(){
@@ -25,6 +33,10 @@ void loop(){
 
     DHT.read11(dht_apin);
     int Moisture = analogRead(soil_apin);
+
+    t_2 = millis();
+    t_min = t_2 - t_1;
+    t_min = (t_min/1000)/60;
     
     Serial.print("Humedad actual = ");
     Serial.println(Moisture);
@@ -33,6 +45,13 @@ void loop(){
     Serial.print(DHT.temperature);
     Serial.println("°C");
 
+    Serial.print("t_1: ");
+    Serial.println(t_1);
+    Serial.print("t_2: ");
+    Serial.println(t_2);
+    Serial.print("t_min: ");
+    Serial.println(t_min);   
+    
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("Humedad:"); 
@@ -43,13 +62,23 @@ void loop(){
     lcd.setCursor(9,1);
     lcd.print(DHT.temperature);
     lcd.display();
+    delay(2000);
 
-    delay(3000);// Delay para accesar sensores dentro de 3 segundos
+    if (t_1 != 0){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Ultimo riego"); 
+      lcd.setCursor (0,1);
+      lcd.print("hace: ");
+      lcd.print(t_min);
+      lcd.print(" min");
+      lcd.display();
+    }
 
-    temperatura = DHT.temperatura
+    int temperatura = DHT.temperature;
 
     // Enciendo el abanico cuando temperatura es alta para refrescar
-    // Apago  abanico cuando llega a 30 grados
+    // Apago abanico cuando llega a 30 grados
     if (temperatura >= 35 ){
       digitalWrite(pin_abanico, HIGH);
 
@@ -62,13 +91,17 @@ void loop(){
     // Enciendo la valvula si la humedad es baja y apago cuando alta
 
     if (Moisture >= 800){
-      digitalWrite(pin_agua, HIGH);
-
-    }else if (Moisture = 200){
+      regar_planta();
+    }
+    else if (Moisture <= 200){
       digitalWrite(pin_agua, LOW);
     }
 
+    delay(2000);// Delay para accesar sensores dentro de 2 segundos
 
-
- 
 }//  Fin del programa
+
+void regar_planta(){
+  digitalWrite(pin_agua, HIGH);
+  t_1 = millis();
+  }
